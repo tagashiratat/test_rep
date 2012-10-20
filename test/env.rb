122ad4180f15f17a0ce43f7667e65d534ea7bbbe
Ubuntu <ubuntu@ip-10-128-4-102.ap-northeast-1.compute.internal>
2012-10-20 05:44:02 +0000

    mod test/env.rb
    test/env.rbを修正する。

--- test/env.rb
+++ test/env.rb
@@ -1,9 +1,10 @@
 class Env
   def "/"
     p "#{Time.new}"
+    <h> Hello Ruby: <%= Time.new %></h>
   end
-
   def "/env"
     p "open env: #{Time.new}"
+    <h>Open Env: <%= Time.new %></h>
   end
 end
